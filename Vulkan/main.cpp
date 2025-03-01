48385a23cc05e578cbb79be0ad007ded492a6a3e
Dull <ermilovdima49@gmail.com>
2025-03-01 10:47:45 +0500

    Lesson 3. Create Instance VK

--- Vulkan/main.cpp
+++ Vulkan/main.cpp
@@ -1,4 +1,5 @@
 #include<GLFW/glfw3.h>
+#include<vulkan/vulkan.h>
 
 int main() {
 	glfwInit();
@@ -6,6 +7,23 @@ int main() {
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+
+	VkApplicationInfo appInfo = {};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Vulkan";
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.pEngineName = nullptr;
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_4;
+
+	VkInstanceCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+
+
+	VkInstance instance = {};
+
+	vkCreateInstance(&createInfo, nullptr, &instance);
 
 	while (glfwWindowShouldClose(window) == false) {
 		glfwPollEvents();
